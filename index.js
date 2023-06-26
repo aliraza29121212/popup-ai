511a72fea49d30c2a8f7cbf9cdb17fe0276f258f
Ali Raza <95703064+aliraza29121212@users.noreply.github.com>
2023-06-26 05:37:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-$(document).ready(() => {
-    let currentStep = 1;
-  
-    $('#nextBtn').click(() => {
+$(document).ready(function(){
+    var currentStep = 1;
+    
+    $('#nextBtn').click(function(){
       currentStep++;
       showStep(currentStep);
     });
-  
-    $('#prevBtn').click(() => {
+    
+    $('#prevBtn').click(function(){
       currentStep--;
       showStep(currentStep);
     });
-  
-    const showStep = (step) => {
+    
+    function showStep(step) {
       $('#step1').hide();
       $('#step2').hide();
       $('#step3').hide();
       $('#editBtn').hide();
-  
+      $('#nextBtn').show();
+      
       if (step === 1) {
         $('#step1').show();
         $('#prevBtn').hide();
@@ -28,12 +29,13 @@ $(document).ready(() => {
         $('#nextBtn').hide();
         $('#editBtn').show();
       }
-    };
-  
-    $('#editBtn').click(() => {
+    }
+    
+    $('#editBtn').click(function(){
       // Perform any action you want when the "Start Editing" button is clicked
       // For example, you can redirect to the editing page or trigger a function to enable editing mode
       // Replace the alert with your desired action
       alert('Editing mode activated!');
     });
   });
+  
